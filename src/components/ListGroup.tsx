7f68ed45c4e99d7d228bbf5dd6fcad56a566bf58
Judy Ann Martos <judyannmartos@Judy-Ann-MacBook-Air.local>
2025-12-06 22:14:18 +0800

    Initial commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,23 @@
+
+
+function ListGroup() {
+const items = ['New York', 'San Francisco',  'Tokyo', 'London',  'Paris'];
+    
+   
+   
+    
+   
+
+
+  return (
+    <>
+        <h1> List</h1>
+        <ul className="list-group">
+         { items.map((items) => (
+          <li key={items} >{items}</li>))}
+         
+        </ul>
+    </>
+  );
+}
+export default ListGroup;
